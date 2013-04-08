acf56cd09afe14666818d1c526f9c746612f619b
Christian Schulte <cs@schulte.it>
2013-04-08 17:56:26 +0000

    o Merged changes up to 4759 from 'trunk' into branch 'jomc-1.x'.
    See #36
    
    
    
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/branches/jomc-1.x/jomc-modlet@4760 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/modlet/DefaultModletProvider.java
+++ src/main/java/org/jomc/modlet/DefaultModletProvider.java
@@ -529,7 +529,12 @@ public class DefaultModletProvider implements ModletProvider
 
     private static String getMessage( final Throwable t )
     {
-        return t != null ? t.getMessage() != null ? t.getMessage() : getMessage( t.getCause() ) : null;
+        return t != null
+               ? t.getMessage() != null && t.getMessage().trim().length() > 0
+                 ? t.getMessage()
+                 : getMessage( t.getCause() )
+               : null;
+
     }
 
 }
