11c4a8785538a3220acbf627375fc6c84a2008f0
Christian Schulte <cs@schulte.it>
2014-02-17 17:59:04 +0000

    o Updated to correct copyright year.
    See #50
    
    
    
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-modlet@4870 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/modlet/ModletProcessor.java
+++ src/main/java/org/jomc/modlet/ModletProcessor.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) Christian Schulte, 2013-005
+ *   Copyright (C) Christian Schulte, 2014-005
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
