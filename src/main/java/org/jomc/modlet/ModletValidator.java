3902f0bfe0bdc42c8f2335ee95e9ee105c141f82
Christian Schulte <cs@schulte.it>
2015-05-27 06:37:19 +0000

    o Re-added my email address to any copyright statements.
    See #78
    
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-modlet@5038 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/modlet/ModletValidator.java
+++ src/main/java/org/jomc/modlet/ModletValidator.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) Christian Schulte, 2015-006
+ *   Copyright (C) Christian Schulte <cs@schulte.it>, 2015-006
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
